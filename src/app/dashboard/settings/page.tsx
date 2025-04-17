8460b8cefafa9bd637ec7fbb17795ea8f2c3a8bf
yashchhatrala25 <yashchhatrala25@gmail.com>
2025-04-17 23:24:53 +0530

    setting page ui complete

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Settings = () => {
   return (
-    <div>Settings</div>
-  )
-}
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 via-indigo-100 to-purple-200 p-10">
+      <div className="max-w7xl mx-auto bg-white rounded-lg shadow-2xl p-8">
+        <h1 className="text-4xl font-extrabold text-gray-900 mb-6">Settings</h1>
+        <p className="text-lg text-gray-700 mb-8 max-w-2xl">
+          Customize your account settings, Profile, and Notifications here.
+          Choose an option from the sidebar to begin
+        </p>
 
-export default Settings
+        <div className="flex space-x-12">
+          <div>
+            <h2 className="text-2xl font-semibold text-gray-800 mv-4">
+              Quicks Links
+            </h2>
+            <ul className="space-y-4 mt-4">
+              <li>
+                <Link
+                  className="flex items-center text-lg text-gray-800 hover:text-indigo-600 transition-colors py-2 px-4 rounded-md hover:bg-indigo-50"
+                  href={"/dashboard/settings/account"}
+                >
+                  <span>Account</span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="flex items-center text-lg text-gray-800 hover:text-yellow-600 transition-colors py-2 px-4 rounded-md hover:bg-yellow-50"
+                  href={"/dashboard/settings/notifications"}
+                >
+                  <span>Notifications</span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="flex items-center text-lg text-gray-800 hover:text-green-600 transition-colors py-2 px-4 rounded-md hover:bg-green-50"
+                  href={"/dashboard/settings/profile"}
+                >
+                  <span>Profile</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="flex-1 bg-white shadow-md rounded-lg p-8">
+            <h2 className="text-3xl font-semibold text-gray-900 mb-6">
+              Select a Settings
+            </h2>
+            <p className="text-lg text-gray-600 max-w-3xl">
+              Choose an option from the sidebar to manage your account,
+              notifications, or profile settings
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
